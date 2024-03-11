7aa4f3acbda7c16feac8a228c90b32f37d35c2ed
bin9638 <vinhkhanhbin@gmail.com>
2024-03-11 18:30:41 +0700

    Fix

--- SortC++.cpp
+++ SortC++.cpp
@@ -68,25 +68,6 @@ const int d8x[8] = {0, 1, 1, 1, 0, -1, -1, -1};
 const int d8y[8] = {1, 1, 0, -1, -1, -1, 0, 1};
 
 
-
-
-// #define LENGTH 1000005
-// #define NMOD 2
-// #define BASE 256
-// const int HashMod[] = {(int)1e9 + 7, (int)1e9 + 2277, (int)1e9 + 5277, (int)1e9 + 8277, (int)1e9 + 9277};
-
-// #include <ext/pb_ds/assoc_container.hpp>
-// #include <ext/pb_ds/tree_policy.hpp>
-// using namespace __gnu_pbds;
-// #define o_set tree<int, null_type,less<int>, rb_tree_tag, tree_order_statistics_node_update>
-// *(s.find_by_order(2)) : 3rd element in the set i.e. 6
-// s.order_of_key(25) : Count of elements strictly smaller than 25 is 4
-
-
-
-
-/* Listen music of IU before enjoy my code */
-
 const int LimN = 5e5 + 5;
 
 int n, a[LimN];
@@ -96,11 +77,6 @@ void solve() {
     sort(a + 1, a + 1 + n);
 
 }
-
-
-/* Authors: Nguyen Minh Huy from Le Quy Don high school for Gifted Students Da Nang */
-
-
 
 signed main() {
 
